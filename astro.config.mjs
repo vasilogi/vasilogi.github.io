8fa4f8ed39c526f65cb8cf22d0d23eb0c8e43d1b
Giannis Vasilopoulos <61663739+vasilogi@users.noreply.github.com>
2025-01-11 19:36:22 +0200

    remove slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vasilogi.github.io/',
+    site: 'https://vasilogi.github.io',
     integrations: [
         mdx(),
         sitemap(),
