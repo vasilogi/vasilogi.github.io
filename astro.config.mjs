287d2abc18de393ed5a912d7f5d6546dce2ded1a
Giannis Vasilopoulos <61663739+vasilogi@users.noreply.github.com>
2025-03-18 21:26:33 +0200

    add google analytics (#23)
    * update automerge and README
    
    * auto-merge
    
    * just PR automated creation
    
    * add auto-merge
    
    * delete automerge
    
    * update README
    
    * delete photos
    
    * update README
    
    * add google analytics ID

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
         sitemap(),
         tailwind({
             applyBaseStyles: false
+        }),
+        partytown({
+            config: {
+                forward: ['dataLayer.push']
+            }
         })
     ]
 });
